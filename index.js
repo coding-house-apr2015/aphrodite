db6ec88e607813d04ffdf0231bea29b1ad6e22be
Chyld Medford <chyld.medford@gmail.com>
2015-05-07 10:56:46 -0700

    refactored for heroku

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ var Server = require('./lib/server');
 Server.init(function(err, server){
   Hoek.assert(!err, err);
   console.log('Hapi:', server.info.uri);
-  console.log('Mongo:', server.app.environment.MONGO_URL);
+  console.log('Environment:', server.app.environment);
 });
