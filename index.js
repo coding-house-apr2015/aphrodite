7e0c44be143c1efc4f5ecdb27f47ac96a43d6fe3
Chyld Medford <chyld.medford@gmail.com>
2015-05-07 16:32:13 -0700

    user auth

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var Server = require('./lib/server');
 
 Server.init(function(err, server){
   Hoek.assert(!err, err);
-  console.log('Hapi:', server.info.uri);
-  console.log('Environment:', server.app.environment);
+  console.log('--------------------------------------------------------------------------------');
+  console.log('Hapi:\n', server.info.uri);
+  console.log('Environment:\n', server.app.environment);
+  console.log('--------------------------------------------------------------------------------');
 });
